8c3cd8d8f9e2ffb5cf3673a23f83a140ba2ec799
Radhika Yusuf Alifiansyah <radhika.yusuf1@gmail.com>
2016-10-06 12:18:16 +0700

    Edit MainActivityVM

--- app/src/main/java/com/example/root/calculatormvvm/ViewModel/MainActivityVM.java
+++ app/src/main/java/com/example/root/calculatormvvm/ViewModel/MainActivityVM.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.root.calculatormvvm.Function.ConvertIdtoString;
+import com.example.root.calculatormvvm.Function.DetectInputType;
 import com.example.root.calculatormvvm.ObservableString;
 
 
@@ -17,13 +19,31 @@ public class MainActivityVM extends GitsVM{
 
     public Button.OnClickListener btn;
     public ObservableString observableString = new ObservableString("");
+    ConvertIdtoString convertIdtoString = new ConvertIdtoString();
+    DetectInputType detectInputType = new DetectInputType();
+
 
     public MainActivityVM(Context context) {
         super(context);
         btn = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String hasilID = convertIdtoString.convertIdtoString(v);
+                String hasilType = detectInputType.detectInputType(hasilID);
 
+                if(hasilType.equals("num")){
+                    observableString.setValue(observableString.getValue()+hasilID);
+                }else if(hasilType.equals("op")){
+                    observableString.setValue(observableString.getValue()+hasilID);
+                }else if(hasilType.equals("clear")){
+
+                }else if(hasilType.equals("his")){
+
+                }else if(hasilType.equals("del")){
+
+                }else if(hasilType.equals("res")){
+
+                }
             }
         };
     }
