5a7ee3e1ba2261e95f2b1a908dffff29f6b3f2c5
aqiel <maqiel2600@gmail.com>
2016-10-06 11:58:15 +0700

    add variable on xml and add bind on obserablestring.class

--- app/src/main/java/com/example/root/calculatormvvm/ObservableString.java
+++ app/src/main/java/com/example/root/calculatormvvm/ObservableString.java
@@ -1,21 +1,26 @@
 package com.example.root.calculatormvvm;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
 /**
  * Created by root on 06/10/16.
  */
 
-public class ObservableString {
+public class ObservableString extends BaseObservable {
     public String value;
 
     public ObservableString(String value) {
         this.value = value;
     }
 
+    @Bindable
     public String getValue() {
         return value;
     }
 
     public void setValue(String value) {
         this.value = value;
+        notifyPropertyChanged(BR.value);
     }
 }
