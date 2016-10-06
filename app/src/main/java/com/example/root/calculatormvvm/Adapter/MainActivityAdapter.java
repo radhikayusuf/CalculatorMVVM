6a21eb01d66a35a4e6489d5f4b646d2ae8461ef0
aqiel <maqiel2600@gmail.com>
2016-10-06 12:50:28 +0700

    add card viw + recycle view MVVM

--- app/src/main/java/com/example/root/calculatormvvm/Adapter/MainActivityAdapter.java
+++ app/src/main/java/com/example/root/calculatormvvm/Adapter/MainActivityAdapter.java
@@ -3,6 +3,7 @@ package com.example.root.calculatormvvm.Adapter;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.root.calculatormvvm.Dao.HistoryDao;
+import com.example.root.calculatormvvm.R;
 import com.example.root.calculatormvvm.ViewModel.HistoryVM;
 import com.example.root.calculatormvvm.databinding.CardHistoryBinding;
 
@@ -23,17 +24,17 @@ public class MainActivityAdapter extends GitsAdapter<HistoryDao,HistoryVM,CardHi
 
     @Override
     public HistoryVM createViewModel(AppCompatActivity activity, CardHistoryBinding binding, HistoryDao item, int position) {
-        return new HistoryVM();
+        return new HistoryVM(activity,binding,item);
     }
 
     @Override
     public int getLayoutRes() {
-        return 0;
+        return R.layout.card_history;
     }
 
     @Override
     public void render(CardHistoryBinding binding, HistoryVM viewModel, HistoryDao item) {
-
+        binding.setHis(viewModel);
     }
 
     @Override
--- app/src/main/java/com/example/root/calculatormvvm/ViewModel/HistoryVM.java
+++ app/src/main/java/com/example/root/calculatormvvm/ViewModel/HistoryVM.java
@@ -1,15 +1,23 @@
 package com.example.root.calculatormvvm.ViewModel;
 
 import android.content.Context;
+import android.databinding.ObservableField;
 
+import com.example.root.calculatormvvm.Dao.HistoryDao;
+import com.example.root.calculatormvvm.databinding.CardHistoryBinding;
+
+import id.gits.mvvmcore.viewmodel.GitsRowVM;
 import id.gits.mvvmcore.viewmodel.GitsVM;
 
 /**
  * Created by Varokah on 10/6/2016.
  */
 
-public class HistoryVM extends GitsVM{
-    public HistoryVM(Context context) {
-        super(context);
+public class HistoryVM extends GitsRowVM<HistoryDao,CardHistoryBinding>{
+    ObservableField<String> bHistoryName = new ObservableField<>();
+    public HistoryVM(Context context, CardHistoryBinding binding, HistoryDao data) {
+        super(context, binding, data);
+        bHistoryName.set(data.getHistory());
+
     }
 }
