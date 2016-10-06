f3e2d5f4d3a8aca909eb5f34075b9b72d82bfaea
aqiel <maqiel2600@gmail.com>
2016-10-06 11:48:37 +0700

    add function convertIdtoString and return string

--- app/src/main/java/com/example/root/calculatormvvm/Function/ConvertIdtoString.java
+++ app/src/main/java/com/example/root/calculatormvvm/Function/ConvertIdtoString.java
@@ -2,14 +2,35 @@ package com.example.root.calculatormvvm.Function;
 
 import android.view.View;
 
+import com.example.root.calculatormvvm.R;
+
 /**
  * Created by Varokah on 10/6/2016.
  */
 
 public class ConvertIdtoString {
-    public  String convertIdtoString(View v){
+    public String convertIdtoString(View v){
         switch (v.getId()){
-            default: return "";
+            case R.id.btn0: return "0";
+            case R.id.btn1: return "1";
+            case R.id.btn2: return "2";
+            case R.id.btn3: return "3";
+            case R.id.btn4: return "4";
+            case R.id.btn5: return "5";
+            case R.id.btn6: return "6";
+            case R.id.btn7: return "7";
+            case R.id.btn8: return "8";
+            case R.id.btn9: return "9";
+            case R.id.btnBagi: return "/";
+            case R.id.btnPlus: return "+";
+            case R.id.btnMin: return "-";
+            case R.id.btnKali: return "*";
+            case R.id.btnEquals: return "=";
+            case R.id.btnDot: return ".";
+            case R.id.btnDel: return "del";
+            case R.id.btnClear: return "clear";
+            case R.id.btnHis: return "his";
+            default: return "none";
         }
     }
 }
