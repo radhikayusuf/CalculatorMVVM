867c36ab2a01b1b04ae0b052eb413f3a70fa9427
aqiel <maqiel2600@gmail.com>
2016-10-06 11:54:00 +0700

    add function DetectInputType and return string

---
+++ app/src/main/java/com/example/root/calculatormvvm/Function/DetectInputType.java
@@ -0,0 +1,32 @@
+package com.example.root.calculatormvvm.Function;
+
+/**
+ * Created by Varokah on 10/6/2016.
+ */
+
+public class DetectInputType {
+    public String detectInputType(String string){
+        switch (string){
+            case "1": return "num";
+            case "2": return "num";
+            case "3": return "num";
+            case "4": return "num";
+            case "5": return "num";
+            case "6": return "num";
+            case "7": return "num";
+            case "8": return "num";
+            case "9": return "num";
+            case "0": return "num";
+            case "+": return "op";
+            case "-": return "op";
+            case "*": return "op";
+            case "/": return "op";
+            case "=": return "res";
+            case "clear": return "clear";
+            case "his": return "his";
+            case "del": return "del";
+            case ".": return "num";
+            default:return "none";
+        }
+    }
+}
