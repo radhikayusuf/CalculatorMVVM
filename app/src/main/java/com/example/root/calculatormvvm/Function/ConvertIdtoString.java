a55bac954c22c08dd86c778584ed672eafeaa5b7
aqiel <maqiel2600@gmail.com>
2016-10-06 11:39:22 +0700

    add function convertIdtoString

---
+++ app/src/main/java/com/example/root/calculatormvvm/Function/ConvertIdtoString.java
@@ -0,0 +1,15 @@
+package com.example.root.calculatormvvm.Function;
+
+import android.view.View;
+
+/**
+ * Created by Varokah on 10/6/2016.
+ */
+
+public class ConvertIdtoString {
+    public static void convertIdtoString(View v){
+        switch (v.getId()){
+
+        }
+    }
+}
